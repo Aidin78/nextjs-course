a6114471e571526a6b286134ca88c9a79fb7a716
aidin78 <sahebi.aidin78@gmail.com>
2026-04-19 15:40:19 +0330

    add back button to image

--- 03-routing-rendering/app/news/[slug]/@modal/(.)image/page.js
+++ 03-routing-rendering/app/news/[slug]/@modal/(.)image/page.js
@@ -1,15 +1,19 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function ImagePage({ params }) {
     const newsItemSlug = params.slug;
     const newsItem = DUMMY_NEWS.find(item => item.slug == newsItemSlug);
+    const router = useRouter();
 
     if (!newsItem) notFound();
 
+
     return (
         <>
-            <div className="modal-backdrop"></div>
+            <div className="modal-backdrop" onClick={router.back}></div>
             <dialog className="modal" open>
                 <div className="fullscreen-image">
                     <img src={`/images/news/${newsItem.image}`}
